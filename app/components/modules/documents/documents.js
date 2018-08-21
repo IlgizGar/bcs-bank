18cd7bea60df07ab73ae0005b110c9838f3c2811
Журавлев Андрей <juravlev@aicrobotics.ru>
2018-08-21 16:30:57 +0300

    Доработан фильтр документов

--- app/components/modules/documents/documents.js
+++ app/components/modules/documents/documents.js
@@ -15,21 +15,67 @@ module.exports = (elem) => {
 
     applyFilter(year) {
       this.groups.each((i, el) => {
+        if ($(el).find('.collapse').length) {
+          DocumentsFilter.checkCollapse($(el).find('.collapse'), year);
+          const items = $(el).find('.collapse .collapse__item:not(.state_hidden)');
+          items.last().addClass('state_no-border');
+        } else {
+          const $docs = $(el).find('[data-documents-year]');
+          const $actual = $(el).find(`[data-documents-year="${year}"]`);
+          if (year.length) {
+            $docs.addClass('state_hidden');
+            if ($actual.length) {
+              $(el).removeClass('state_hidden');
+              $actual.removeClass('state_hidden');
+            } else {
+              $(el).addClass('state_hidden');
+            }
+          } else {
+            $(el).removeClass('state_hidden');
+            $docs.removeClass('state_hidden');
+          }
+        }
+      });
+
+      $('.js-tab').each((i, el) => {
+        if ($(el).find('.js-documents-group:not(.state_hidden)').length) {
+          $(el).find('.documents__no-result').addClass('state_hidden');
+        } else {
+          // $(el).find('.documents__no-result').remove();
+          $(el).append('<div class="documents__no-result">\n' +
+            '                    <p>Документов этого года нет.</p>\n' +
+            '                    <p>Попробуйте выбрать другой год.</p>\n' +
+            '                  </div>');
+        }
+      });
+    }
+
+    static checkCollapse($collapse, year) {
+      const $items = $collapse.find('.collapse__content');
+      $items.each((i, el) => {
         const $docs = $(el).find('[data-documents-year]');
         const $actual = $(el).find(`[data-documents-year="${year}"]`);
+
         if (year.length) {
-          $docs.addClass('state_hidden');
           if ($actual.length) {
-            $(el).removeClass('state_hidden');
+            $(el).closest('.collapse__item').removeClass('state_hidden');
+            $docs.addClass('state_hidden');
             $actual.removeClass('state_hidden');
           } else {
-            $(el).addClass('state_hidden');
+            $(el).closest('.collapse__item').addClass('state_hidden');
           }
         } else {
-          $(el).removeClass('state_hidden');
+          $(el).closest('.collapse__item').removeClass('state_hidden');
+          $docs.closest('.collapse__item').removeClass('state_no-border');
           $docs.removeClass('state_hidden');
         }
       });
+
+      if (!$collapse.find('.collapse__item:not(.state_hidden)').length) {
+        $collapse.closest('.documents__group').addClass('state_hidden');
+      } else {
+        $collapse.closest('.documents__group').removeClass('state_hidden');
+      }
     }
   }
 
