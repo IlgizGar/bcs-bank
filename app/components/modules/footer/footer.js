2cfdc20ed1d3dbb60c996889fe5b151b0dfed6c8
Andrej Zhuravlev <juravlev@aicrobotics.ru>
2018-09-24 18:43:22 +0300

    Адаптив офисов и банкоматов.

--- app/components/modules/footer/footer.js
+++ app/components/modules/footer/footer.js
@@ -3,5 +3,15 @@ import $ from 'jquery';
 export default class Footer {
   constructor() {
     this.footer = $('.js-footer');
+    this.menu = this.footer.find('.js-footer-menu');
+
+    this.events();
+  }
+
+  events() {
+    this.menu.on('click', (e) => {
+      $(e.currentTarget).find('.js-footer-menu-content').slideToggle(255);
+      $(e.currentTarget).toggleClass('state_explored');
+    });
   }
 }
