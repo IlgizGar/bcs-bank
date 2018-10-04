4ea7824f97a66728bc2bed2aa28bf1343770c978
Andrej Zhuravlev <juravlev@aicrobotics.ru>
2018-10-04 18:27:35 +0300

    Верстка и добавление логики к слайдеру input

--- app/components/library/input/input.js
+++ app/components/library/input/input.js
@@ -1,54 +1,81 @@
 import $ from 'jquery';
-// import Slider from 'nouislider';
+import 'ion-rangeslider';
 
 module.exports = (elem) => {
-  class Input {
-    constructor(selector) {
-      this.input = $(selector);
-      this.field = this.input.find('input, textarea');
+    class Input {
+        constructor(selector) {
+            this.input = $(selector);
+            this.field = this.input.find('.js-input-field');
+            this.ionSlider = null;
 
-      this.init();
-      this.events();
+            this.init();
+            this.events();
+        }
+
+        init() {
+            this.slider = this.input.find('.js-input-slider');
+            if (this.slider.length) {
+                this.slider.ionRangeSlider({
+                    type: 'single',
+                    min: this.slider.data('min'),
+                    max: this.slider.data('max'),
+                    from: this.slider.data('from'),
+                    step: this.slider.data('step'),
+                    prettify_enabled: true,
+                    onChange: (data) => {
+                        this.field.val(data.from);
+                    }
+                });
+                this.field.val(this.slider.data('from'));
+                this.input.removeClass('state_init');
+
+                this.ionSlider = this.slider.data('ionRangeSlider');
+            }
+        }
+
+        events() {
+            this.input.on('click', () => {
+                if (this.input.hasClass('state_init')) {
+                    this.input.removeClass('state_init');
+                    this.field.focus();
+                }
+            });
+
+            this.field.on('blur', () => {
+                if (!this.input.hasClass('state_filled')) {
+                    this.input.addClass('state_init');
+                } else {
+                    if(this.field.val().length < 1) {
+                        if (this.ionSlider !== null) {
+                            console.log('FROM', this.slider.data('from'));
+
+                            this.field.val(this.slider.data('from'));
+                            this.ionSlider.update({from: this.slider.data('from')})
+                        }
+                    }
+                }
+            });
+
+            this.field.on('keyup', () => {
+
+                if (this.field.val().length < 1) {
+                    if (this.ionSlider === null) {
+                        this.input.removeClass('state_filled');
+                    }
+                } else {
+                    if (!this.input.hasClass('state_filled')) {
+                        this.input.addClass('state_filled');
+                    }
+
+                    if (this.ionSlider !== null) {
+                        this.ionSlider.update({
+                            from: parseFloat(this.field.val().replace(/ /g, ''))
+                        });
+                    }
+                }
+            });
+        }
     }
 
-    init() {
-      this.slider = this.input.find('.js-input-slider');
-      if (this.slider.length) {
-        // Slider.create(this.slider, {
-        //   start: [20, 80],
-        //   connect: true,
-        //   range: {
-        //     'min': 0,
-        //     'max': 100
-        //   }
-        // });
-        // this.slider.slider();
-      }
-    }
-
-    events() {
-      this.input.on('click', () => {
-        if (this.input.hasClass('state_init')) {
-          this.input.removeClass('state_init');
-          this.field.focus();
-        }
-      });
-
-      this.field.on('blur', () => {
-        if (!this.input.hasClass('state_filled')) {
-          this.input.addClass('state_init');
-        }
-      });
-
-      this.field.on('keyup', () => {
-        if (this.field.val().length < 1) {
-          this.input.removeClass('state_filled');
-        } else if (!this.input.hasClass('state_filled')) {
-          this.input.addClass('state_filled');
-        }
-      });
-    }
-  }
-
-  return new Input(elem);
+    return new Input(elem);
 };
