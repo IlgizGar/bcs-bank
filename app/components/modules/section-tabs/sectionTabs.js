739daf8082d38063293d188a116f470e6ab4f491
max <mmx_zmax@mail.ru>
2019-04-25 12:26:55 +0300

    page usefull animation fix

--- app/components/modules/section-tabs/sectionTabs.js
+++ app/components/modules/section-tabs/sectionTabs.js
@@ -5,8 +5,28 @@ module.exports = (elem) => {
     constructor(selector) {
       this.tabsContainer = $(selector);
       this.tab = this.tabsContainer.find('[data-tab-show]');
+      this.marker = $(this.createTabbarMarker());
       this.init();
       this.events();
+    }
+
+    createTabbarMarker() {
+      const marker = document.createElement('div');
+      marker.classList.add('tabbar__marker');
+      marker.style.color = '#3b66c5';
+      marker.style.bottom = '0';
+      marker.style.top = 'auto';
+      this.tab.parent()[0].appendChild(marker);
+      return marker;
+    }
+
+    setMarkerPos(activeAnchor) {
+      const width = activeAnchor.width();
+      const position = activeAnchor.position();
+      const props = {};
+      props.width = width;
+      props.transform = `translate3d(${position.left}px, 0, 0)`;
+      this.marker.css(props);
     }
 
     init() {
@@ -14,7 +34,9 @@ module.exports = (elem) => {
     }
     showTab(id) {
       this.tab.removeClass('state_active');
-      this.tabsContainer.find(`[data-tab-show="${id}"]`).addClass('state_active');
+      const activeTab = this.tabsContainer.find(`[data-tab-show="${id}"]`);
+      activeTab.addClass('state_active');
+      this.setMarkerPos(activeTab);
       this.tab.each((index, element) => {
         const tab = $($(element).attr('data-tab-show'));
         tab.addClass('state_hidden');
