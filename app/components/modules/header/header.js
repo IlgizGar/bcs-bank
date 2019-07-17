8ab4f615381a8ae0a6bafe7a8d7ac93fa2df709f
max <mmx_zmax@mail.ru>
2019-07-17 15:36:07 +0300

    refactor header script

--- app/components/modules/header/header.js
+++ app/components/modules/header/header.js
@@ -8,6 +8,9 @@ module.exports = (elem) => {
       this.btnContacts = this.header.find('.js-btn-header-contacts');
       this.btnOffices = this.header.find('.js-btn-header-offices');
       this.btnOfficesState = false;
+      this.activeClass = 'state_active';
+      this.exploredClass = 'state_explored';
+      this.unscrollClass = 'state_unscroll';
 
       this.mobileMenu = this.header.find('.js-mobile-menu');
       this.exploreMenu = $('#menu-explore');
@@ -24,23 +27,23 @@ module.exports = (elem) => {
       this.btnContacts.on('click', (e) => {
         this.btnContacts = this.header.find('.js-btn-header-contacts'); // Не удалять! Требуется для синхронизации мобильных и десктопных версий меню.
 
-        if (!$(e.currentTarget).hasClass('state_active')) {
-          this.btnContacts.addClass('state_active');
-          this.contacts.addClass('state_explored');
+        if (!$(e.currentTarget).hasClass(this.activeClass)) {
+          this.btnContacts.addClass(this.activeClass);
+          this.contacts.addClass(this.exploredClass);
           $('main').append('<div class="page__cover js-cover"></div>');
-          $('body').addClass('state_unscroll');
+          $('body').addClass(this.unscrollClass);
           $('.js-navbar').removeClass('state_init');
-          if (this.btnOffices.hasClass('state_active')) {
+          if (this.btnOffices.hasClass(this.activeClass)) {
             this.btnOfficesState = true;
-            this.btnOffices.removeClass('state_active');
+            this.btnOffices.removeClass(this.activeClass);
           }
         } else {
-          this.btnContacts.removeClass('state_active');
-          this.contacts.removeClass('state_explored');
+          this.btnContacts.removeClass(this.activeClass);
+          this.contacts.removeClass(this.exploredClass);
           $('main').find('.js-cover').remove();
-          $('body').removeClass('state_unscroll');
+          $('body').removeClass(this.unscrollClass);
           if (this.btnOfficesState) {
-            this.btnOffices.addClass('state_active');
+            this.btnOffices.addClass(this.activeClass);
             this.btnOfficesState = false;
           } else {
             $('.js-navbar').addClass('state_init');
@@ -50,27 +53,18 @@ module.exports = (elem) => {
 
       $(window).on('click', (e) => {
         if ($(e.target).closest('.js-cover').length) {
-          this.btnContacts.removeClass('state_active');
-          this.contacts.removeClass('state_explored');
-          $('body').removeClass('state_unscroll');
+          this.btnContacts.removeClass(this.activeClass);
+          this.contacts.removeClass(this.exploredClass);
+          $('body').removeClass(this.unscrollClass);
           $(e.target).remove();
         }
       });
-
-      $(window).on('resize', () => {
-        // if (window.innerWidth < 1280) {
-        //   if (!this.mobileMenu.hasClass('state_filled')) {
-        //     this.fillMobileMenu();
-        //   }
-        // }
-      });
-
       $('html').keydown((e) => {
         if (e.keyCode === 27) {
-          this.btnContacts.removeClass('state_active');
-          this.contacts.removeClass('state_explored');
+          this.btnContacts.removeClass(this.activeClass);
+          this.contacts.removeClass(this.exploredClass);
           $('main').find('.js-cover').remove();
-          $('body').removeClass('state_unscroll');
+          $('body').removeClass(this.unscrollClass);
         }
       });
     }
@@ -80,14 +74,15 @@ module.exports = (elem) => {
       const $nav = $.extend(true, {}, $('.js-nav').clone());
       const $online = $.extend(true, {}, $('.js-online-bank').clone());
       const $explore = $.extend(true, {}, this.exploreMenu.clone());
+      const linkClass = 'button_theme-black mobile-menu__link';
 
       $nav.appendTo(this.mobileMenu);
       $menu.find('.js-button')
-        .removeClass('state_active')
-        .addClass('button_theme-black mobile-menu__link');
+        .removeClass(this.activeClass)
+        .addClass(linkClass);
       $menu.appendTo(this.mobileMenu);
       $explore.find('a')
-        .addClass('button button_view-text button_size-low menu__item js-button button_theme-black mobile-menu__link')
+        .addClass(`button button_view-text button_size-low menu__item js-button ${linkClass}`)
         .appendTo(this.mobileMenu.find('.js-menu .menu__wrapper'));
       $online
         .addClass('button_theme-default mobile-menu__online-bank')
