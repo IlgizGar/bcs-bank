0582ef86dae260e4e81f9d65b57d61c17880f9db
degtrs <degts@gmail.com>
2019-09-03 11:40:19 +0300

    Переделал попап и прописал стили для десктопа

--- app/components/modules/partners-modal/partners-modal.js
+++ app/components/modules/partners-modal/partners-modal.js
@@ -5,6 +5,7 @@ module.exports = (elem) => {
     constructor(selector) {
       this.modal = $('.js-partners-modal');
       this.title = this.modal.find('.js-partners-modal-title span');
+      this.subscriptionText = this.modal.find('.js-partners-modal-subscription-text');
       this.text = this.modal.find('.js-partners-modal-text');
       this.link = this.modal.find('.js-partners-modal-button');
       this.theme = this.modal.find('.js-partners-modal-theme');
@@ -16,13 +17,14 @@ module.exports = (elem) => {
         event.stopPropagation();
         event.preventDefault();
         const btn = $(event.currentTarget);
-        this.setModalData(btn.data('title'), btn.data('text'), btn.data('link'));
+        this.setModalData(btn.data('title'), btn.data('text'), btn.data('link'), btn.data('subscriptiontext'));
         this.modal.modal();
       });
     }
-    setModalData(title, text, link) {
+    setModalData(title, text, link, subscriptionText) {
       this.title.text(title);
       this.text.text(text);
+      this.subscriptionText.text(subscriptionText);
       this.link.attr('href', link);
       this.theme.val(title);
     }
