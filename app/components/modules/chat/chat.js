1c016fce64aa7f9bb8ae1c6a84e489f5e2bef233
degtrs <degts@gmail.com>
2019-09-04 10:11:38 +0300

    temporary commit

--- app/components/modules/chat/chat.js
+++ app/components/modules/chat/chat.js
@@ -7,13 +7,14 @@ export default class Chat {
   }
 
   init() {
-    window.yandexMapApiKey = '43aae7e6-b7a1-4f85-b4ca-242979d9dcfd';
-    var currentLocation;
-    Helpers.getGeolocation((curLoc) => {
-      // console.log(curLoc);
-      global.currentCity = curLoc.GeocoderMetaData.Address.Components[4].name;
-      console.log('Ваш город: ', global.currentCity);
-    });
+    // window.yandexMapApiKey = '43aae7e6-b7a1-4f85-b4ca-242979d9dcfd';
+    // var currentLocation;
+    // Helpers.getGeolocation((curLoc) => {
+    //   // console.log(curLoc);
+    //   global.currentCity = curLoc.GeocoderMetaData.Address.Components[4].name;
+    //   console.log('Ваш город: ', global.currentCity);
+    //   window.clientChatCity = JSON.stringify(global.currentCity);
+    // });
 
     window.clientData = JSON.stringify({
       "name": "Name Surname",
@@ -21,10 +22,15 @@ export default class Chat {
       "email": "e@mail.com",
       "customField":"customValue"
     });
-    this.initializeChat();
+    // this.initializeChat();
   }
 
   initializeChat() {
     !function(configurationFile){"use strict";configurationFile=configurationFile||"/settings.json";var e=window,t=document;function n(t,n){var a=setInterval(function(){e.ThreadsWidget&&!e.ThreadsWidget.isDummy&&(clearInterval(a),e.ThreadsWidget[t]&&e.ThreadsWidget[t](n))},100)}e.ThreadsWidget={isDummy:!0,showChat:function(){n("showChat")},hideChat:function(){n("hideChat")},onHideChat:function(e){n("onHideChat",e)},onScenarios:function(e){n("onScenarios",e)}};var a,s=(a=new XMLHttpRequest,function(e,t,n,s){a.onreadystatechange=function(){if(4===a.readyState)if(200===this.status)n(a.response);else{if("function"!=typeof s)throw new Error(a.response);s(a)}},a.open(e,t),a.send()});function i(e){if(e.webchat&&(e.webchat.filename=e.filename),e.style&&(e.webchat.style=e.style),sessionStorage.setItem("__threadsWidget",JSON.stringify(e.webchat)),e.filename){var n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src=e.filename;var a=t.getElementsByTagName("script")[0];a?a.parentNode.insertBefore(n,a):t.body.appendChild(n)}else console&&console.error("Invalid bundle");if(e.style){var s=t.createElement("link");s.type="text/css",s.rel="stylesheet",s.href=e.style,t.getElementsByTagName("head")[0].appendChild(s)}}function r(){s("GET",configurationFile+"?rnd="+Math.random(),function(e){var t=JSON.parse(e);i(t)})}"complete"===t.readyState?r():e.attachEvent?e.attachEvent("onload",r):e.addEventListener("load",r,!1)}("/assets/json/settings.json");
   }
+  // chatIFRAMEfuncExample() {
+  // // $("iframe[name='__threadswidget_chat__iframe']").contents().find('h3').css('font-weight', 600)
+  // }
 }
+
+
