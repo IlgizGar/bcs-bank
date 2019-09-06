3e60633e0883a0a89e194a695c5b793ed3b468ac
degtrs <degts@gmail.com>
2019-09-06 13:14:27 +0300

    Последние изменения кода

--- app/components/modules/chat/chat.js
+++ app/components/modules/chat/chat.js
@@ -24,15 +24,94 @@ export default class Chat {
     this.initializeChat();
     $('iframe#__threadswidget_chat__iframe').contents().find('.threadswidget_welcome__form h3').css('font-weight', 600);
     global.chateg = ThreadsWidget;
-    $(document).ready(()=>{
-    });
     $.when(this.initializeChat()).then(function() {
       // console.log($("iframe#__threadswidget_chat__iframe").contents().find('.threadswidget_welcome__form').is(':visible'));
       $('iframe#__threadswidget_chat__iframe').contents().find('.threadswidget_welcome__form h3').css('font-weight', 600);
     })
   }
 
+
   initializeChat() {
+    function doOnChatInit() {
+      let iFrameChat =  $('iframe#__threadswidget_chat__iframe').contents();
+      let cityInput = `<label for="city" data-size=""><input id="city" name="city" maxlength="50" placeholder="Ваш город" value="${window.currentCity ? window.currentCity : ''}" style="font-family: 'Open Sans', sans-serif; margin: 0px 0px 24px; padding: 20px 16px; background-color: rgb(255, 255, 255); color: rgb(0, 25, 52); font-size: 16px; border-radius: 4px; border-color: rgb(216, 216, 216);"></label>`;
+
+      iFrameChat.find('.threadswidget_welcome__form h3').css('font-weight', 600);
+      iFrameChat.find('button.Button').hide();
+      iFrameChat.find('.threadswidget_chat__body___wrapper>.clipper').css('padding-top', 0).css('padding-bottom', 0);
+      iFrameChat.find('.threadswidget_chat__body').css('padding', '24px');
+      iFrameChat.find('.chat__header').css('background', '#ffffff');
+      iFrameChat.find('.threadswidget_chat__welcome').css('padding', '0');
+      iFrameChat.find('.threadswidget_chat__welcome p').css('line-height', '24px').css('padding', '0');
+      iFrameChat.find('.threadswidget_welcome__form div:first-child').css('padding-top', '64px');
+      iFrameChat.find('.threadswidget_welcome__form').css('margin', '0 auto').css('padding', '0 24px').css('max-width', '100%').css('margin', '0').css('top', 0).css('bottom', '100%');
+      iFrameChat.find('label[for="phone"] input').attr('placeholder', 'Номер телефона');
+      iFrameChat.find('label[for="email"] input').val('empty@email.com').hide();
+      iFrameChat.find('.threadswidget_chat__wrapper .icon-close').css('top', '30px').css('right', '30px');
+      iFrameChat.find('.threadswidget_welcome__form div:first-child').after(iFrameChat.find('label[for="name"]'));
+
+      iFrameChat.find('.threadswidget_welcome__form label input').css('margin', '0');
+      iFrameChat.find('.threadswidget_welcome__form label').css('margin', '0 0 24px');
+      iFrameChat.find('label[for="phone"]').after(cityInput);
+      iFrameChat.find('.textareaWrapper').css('max-height', '143px');
+      iFrameChat.find('.textareaWrapper textarea').css('max-height', '143px');
+
+      iFrameChat.find('.threadswidget_welcome__form label input').each(function() {
+        let errorMessage;
+        switch ($(this).attr('name')) {
+          case 'name':
+            errorMessage = 'Укажите Ваше имя';
+            break;
+          case 'phone':
+            errorMessage = 'Укажите ваш телефон';
+            break;
+          case 'city':
+            errorMessage = 'Укажите ваш город';
+            break;
+        }
+        if ($(this).attr('name') != 'email') $(this).after('<span class="chat__error-message" style="position: absolute; display: none; font-size: 12px; padding-left: 16px">Ошибкен</span>');
+      });
+
+      iFrameChat.find('.textareaWrapper textarea').keyup(function(e) {
+          if (localStorage.getItem('chatGotUserInfo') == null && key == "Enter") {
+            e.preventDefault();
+            if (iFrameChat.find('label[for="name"] input').val == '' ||
+              iFrameChat.find('label[for="phone"] input').val == '' ||
+               iFrameChat.find('label[for="city"] input').val == ''
+            ) {
+              e.preventDefault();
+              showError();
+            } else {
+              e.preventDefault();
+              iFrameChat.find('button.Button').trigger('click');
+              localStorage.setItem('chatGotUserInfo', 'true');
+            }
+          }
+        })
+    }
+
+    function showError() {
+      let iFrameChat =  $('iframe#__threadswidget_chat__iframe').contents();
+      iFrameChat.find('.threadswidget_welcome__form label .error-message').remove();
+      iFrameChat.find('.threadswidget_welcome__form label input').each(function() {
+        if ($(this).val == '' && $(this).attr('name') != 'email') {
+          let errorMessage;
+          switch ($(this).attr('name')) {
+            case 'name':
+              errorMessage = 'Укажите Ваше имя';
+              break;
+            case 'phone':
+              errorMessage = 'Укажите ваш телефон';
+              break;
+            case 'city':
+              errorMessage = 'Укажите ваш город';
+              break;
+          }
+          $(this).after('<span class="chat__error-message" style="position: absolute; display: none; font-size: 12px; padding-left: 16px">' + errorMessage + '</span>');
+        }
+      });
+    }
+
     !function (configurationFile, cb) {
       'use strict';
       configurationFile = configurationFile || '/settings.json';
@@ -81,25 +160,7 @@ export default class Chat {
           var a = t.getElementsByTagName('script')[0];
           a.onload = function() {
             $('#__threadswidget_chat__iframe').on('load', () => {
-              let iFrameChat =  $('iframe#__threadswidget_chat__iframe').contents();
-              iFrameChat.find('.threadswidget_welcome__form h3').css('font-weight', 600);
-              iFrameChat.find('button.Button').hide();
-              iFrameChat.find('.threadswidget_chat__body___wrapper>.clipper').css('padding-top', 0).css('padding-bottom', 0);
-              iFrameChat.find('.threadswidget_chat__body').css('padding', '0');
-              iFrameChat.find('.threadswidget_chat__welcome').css('padding', '0');
-              iFrameChat.find('.threadswidget_chat__welcome p').css('line-height', '24px').css('padding', '0');
-              iFrameChat.find('.threadswidget_welcome__form div:first-child').css('padding-top', '64px');
-              iFrameChat.find('.threadswidget_welcome__form').css('margin', '0 auto').css('padding', '0 24px').css('max-width', '100%').css('margin', '0').css('top', 0).css('bottom', '100%');
-              iFrameChat.find('label[for="phone"] input').attr('placeholder', 'Номер телефона');
-              iFrameChat.find('label[for="email"] input').val('empty@email.com').hide();
-              iFrameChat.find('.threadswidget_chat__wrapper .icon-close').css('top', '30px').css('right', '30px');
-              iFrameChat.find('.threadswidget_welcome__form div:first-child').after(iFrameChat.find('label[for="name"]'));
-
-              let cityInput = `<label for="city" data-size=""><input id="city" name="city" maxlength="50" placeholder="Ваш город" value="${window.currentCity ? window.currentCity : ''}" style="font-family: 'Open Sans', sans-serif; margin: 0px 0px 24px; padding: 20px 16px; background-color: rgb(255, 255, 255); color: rgb(0, 25, 52); font-size: 16px; border-radius: 4px; border-color: rgb(216, 216, 216);"></label>`;
-
-              iFrameChat.find('label[for="phone"]').after(cityInput);
-              iFrameChat.find('.textareaWrapper').css('max-height', '143px');
-              iFrameChat.find('.textareaWrapper textarea').css('max-height', '143px');
+              doOnChatInit();
             })
           };
           a ? a.parentNode.insertBefore(n, a) : t.body.appendChild(n);
